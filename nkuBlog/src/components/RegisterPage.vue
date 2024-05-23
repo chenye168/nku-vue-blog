b1b759c2d29efe6e81779b9cf6c2affced35f459
chenye <2305125865@qq.com>
2024-05-23 21:03:48 +0800

    feat:注册页面完成

--- nkuBlog/src/components/RegisterPage.vue
+++ nkuBlog/src/components/RegisterPage.vue
@@ -1,15 +1,115 @@
 <template>
-  <div class="header" v-if="active === 0">Register</div>
 
-  <div class="form-wrapper" v-show="active === 0">
-    <input type="text" name="username" placeholder="username" class="input-item" />
-    <input type="password" name="password" placeholder="password" class="input-item" />
+   <el-form
+    ref="formRef"
+    :model="form"
+    :rules="rules"
+    label-width="100px"
+    class="demo-ruleForm"
+   >
+   <div class="form-wrapper" v-show="active === 0">
+    <div class="header" v-if="active === 0">Register</div>
+    <el-upload
+    class="avatar-uploader"
+    action="http://localhost:5173/api/upload"
+    :show-file-list="false"
+    :on-success="handleAvatarSuccess1"
+    :before-upload="beforeAvatarUpload"
+  >
+    <img v-if="avatarImageUrl" :src="avatarImageUrl" class="avatar" style="width: 90px; height: 90px; border-radius: 100%;">
+    <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+  </el-upload>
+    <el-form-item label="Username" prop="userName">
+      <el-input v-model="form.userName" placeholder="Username" ></el-input>
+    </el-form-item>
+    <el-form-item label="Password" prop="userPassword">
+      <el-input v-model="form.userPassword" placeholder="Password" type="password"></el-input>
+    </el-form-item>
+    <el-form-item label="rePassword" prop="rePassword">
+      <el-input v-model="form.rePassword" placeholder="rePassword" type="password"></el-input>
+    </el-form-item>
+
+    
+  </div>
+  <div class="form-wrapper2" v-show="active === 1">
+    <div class="header" v-if="active === 1">Basic Info</div>
+    <div style="display: flex;margin-bottom: 30px;">
+      <p style="margin-right: 30px; position: fixed; left: 100px">Label</p>
+    <el-input
+      v-if="inputVisible"
+      ref="InputRef"
+      v-model="inputValue"
+      class="w-20"
+      size="small"
+      @keyup.enter="handleInputConfirm"
+      @blur="handleInputConfirm"
+      style="width: 60px;"
+    />
+    <el-button v-else class="button-new-tag" size="small" @click="showInput">
+      + New Tag
+    </el-button>
+    </div>
+   
+    <div class="flex gap-2" style="margin-bottom: 20px;">
+    <el-tag
+      v-for="tag in form.label"
+      :key="tag"
+      closable
+      :disable-transitions="false"
+      @close="handleClose(tag)"
+      style="margin: 2px;"
+    >
+      {{ tag }}
+    </el-tag>
+    
+  </div>
+  
+  <el-form-item label="Address" prop="address">
+      <el-input v-model="form.address" placeholder="Address"></el-input>
+    </el-form-item>
+    <el-form-item label="Mail" prop="mail">
+      <el-input v-model="form.mail" placeholder="Mail"></el-input>
+    </el-form-item>
+  </div>
+  <div class="form-wrapper" v-show="active === 2">
+    <div class="header" v-if="active === 2">Contact Ways</div>
+    <el-form-item label="Github" prop="github">
+      <el-input v-model="form.github" placeholder="Github"></el-input>
+    </el-form-item>
+    <el-form-item label="Wechat ID" prop="wechat.id">
+      <el-input v-model="form.wechat.id" placeholder="Wechat ID"></el-input>
+    </el-form-item>
+    <el-form-item label="Wechat QR Code" prop="wechat.qrCode">
+      <el-upload
+        class="avatar-uploader"
+        action="http://localhost:5173/api/upload"
+        :show-file-list="false"
+        :on-success="handleAvatarSuccess2"
+        :before-upload="beforeAvatarUpload"
+      >
+        <img v-if="wxImageUrl" :src="wxImageUrl" class="avatar" style="width: 100px; height: 100px;" />
+        <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+      </el-upload>
+    </el-form-item>
+  </div>
+   </el-form>
+
+   <!-- 确认页 -->
+  <div class="form-wrapper" v-show="active === 3">
+    <div class="header" v-if="active === 3">Confirm</div>
+    <div >Please confirm your information</div>
+    <!-- 显示表单信息 -->
+    <div class="msg2">Username: {{ form.userName }}</div>
+    <div class="msg2">Password: {{ form.userPassword }}</div>
+    <div class="msg2">Label: {{ form.label }}</div>
+    <div class="msg2">Address: {{ form.address }}</div>
+    <div class="msg2">Mail: {{ form.mail }}</div>
+    <div class="msg2">Github: {{ form.github }}</div>
+    <div class="msg2">Wechat ID: {{ form.wechat.id }}</div>
+
   </div>
 
-  <div class="form-wrapper" v-show="active === 0"></div>
-
-  <div class="form-wrapper" v-show="active === 0"></div>
-
+    
   <div class="step">
     <el-steps style="max-width: 600px" :active="active" finish-status="success">
       <el-step title=" 1" />
@@ -23,9 +123,13 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, defineProps } from 'vue'
+import { ref, defineProps, reactive, nextTick, computed } from 'vue'
 import { registerUser } from '@/api/RegisterUser'
 
+import { ElMessage, ElInput } from 'element-plus'
+import { Plus } from '@element-plus/icons-vue'
+import type { UploadProps } from 'element-plus'
+import type { FormInstance, FormRules } from 'element-plus'
 
 const props = defineProps({
   func: {
@@ -34,13 +138,71 @@ const props = defineProps({
   }
 })
 
+
+const formRef = ref<FormInstance | null>(null)
+const form = ref({
+  userName: '',
+  userPassword: '',
+  rePassword: '',
+  photo: 'default.jpg',
+  label: [] as string[],
+  address: '',
+  mail: '',
+  github: '',
+  wechat: {
+    id: '',
+    qrCode: ''
+  }
+})
+
+
 //第几页
 const active = ref(0)
 //前进后退键值
 const backB = ref('Login')
 const nextB = ref('Next Step')
+
+const validateForm = () => {
+  return new Promise((resolve) => {
+    formRef.value!.validate((valid) => {
+      if (!valid) {
+        ElMessage.error('Please fill in the form correctly');
+      }
+      resolve(valid);
+    });
+  });
+};
+
+async function registe() {
+  let isValid = await validateForm();
+    if (!isValid) {
+      console.log(isValid)
+      return;
+    }
+    
+    //提交表单
+    // 将对象转换为json字符串
+    // 删除rePassword
+    
+    form.value.rePassword = ''
+    registerUser(form.value).then((res) => {
+      if (res.status === 200) {
+        ElMessage.success('Register successfully')
+        props.func()
+      } else {
+        ElMessage.error('Register failed')
+      }
+    })
+}
+
 //按键逻辑
 const next = () => {
+  if (active.value === 3) {
+    //对表单进行验证
+    registe();
+  
+    return
+  }
   if (active.value++ > 1) {
     //更改按键的值
     nextB.value = 'Submit'
@@ -54,6 +216,122 @@ const back = () => {
   nextB.value = 'Next Step'
 }
 
+
+//头像框上传
+
+const updateAvatar = computed(() => {
+  return 'http://src.cycodes.cn/src/img/'+form.value.photo
+})
+
+const updateWxqrcode = computed(() => {
+  if (!form.value.wechat.qrCode) return ''
+  return 'http://src.cycodes.cn/src/img/'+form.value.wechat.qrCode
+})
+
+const avatarImageUrl = ref(updateAvatar)
+const wxImageUrl = ref(updateWxqrcode)
+
+const handleAvatarSuccess1: UploadProps['onSuccess'] = (
+  response
+) => {
+  console.log(response)
+  form.value.photo = response.path;
+}
+
+const handleAvatarSuccess2: UploadProps['onSuccess'] = (
+  response
+) => {
+  console.log(response)
+  form.value.wechat.qrCode = response.path;
+}
+
+const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
+  if (rawFile.type !== 'image/jpeg') {
+    ElMessage.error('Avatar picture must be JPG format!')
+    return false
+  } else if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error('Avatar picture size can not exceed 2MB!')
+    return false
+  }
+  return true
+}
+
+
+
+//表单逻辑
+
+
+// 验证表单下一步
+
+
+
+const rules = reactive<FormRules>({
+  userName: [
+    { required: true, message: 'Please enter your username', trigger: 'blur' }
+  ],
+  userPassword: [
+    { required: true, message: 'Please enter your password', trigger: 'blur' }
+  ],
+  rePassword: [
+    { required: true, message: 'Please enter your rePassword', trigger: 'blur' },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== form.value.userPassword) {
+          callback(new Error('The two passwords do not match'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur'
+    }
+  ],
+  photo: [
+    { required: true, message: 'Please upload your photo', trigger: 'blur' }
+  ],
+  label: [
+    { required: true, message: 'Please enter your label', trigger: 'blur' }
+  ],
+  address: [
+    { required: true, message: 'Please enter your address', trigger: 'blur' }
+  ],
+  mail: [
+    { required: true, message: 'Please enter your mail', trigger: 'blur' }
+  ],
+  github: [
+    { required: true, message: 'Please enter your github', trigger: 'blur' }
+  ],
+  'wechat.id': [
+    { required: true, message: 'Please enter your wechat id', trigger: 'blur' }
+  ],
+  'wechat.qrCode': [
+    { required: true, message: 'Please upload your wechat qrCode', trigger: 'blur' }
+  ]
+})
+
+
+//标签逻辑
+const inputValue = ref('')
+
+const inputVisible = ref(false)
+const InputRef = ref<InstanceType<typeof ElInput>>()
+  const handleClose = (tag: string) => {
+    form.value.label.splice(form.value.label.indexOf(tag), 1)
+}
+
+const showInput = () => {
+  inputVisible.value = true
+  nextTick(() => {
+    InputRef.value!.input!.focus()
+  })
+}
+
+const handleInputConfirm = () => {
+  if (inputValue.value) {
+    form.value.label.push(inputValue.value)
+  }
+  inputVisible.value = false
+  inputValue.value = ''
+}
 // 测试接口
 // const test = {
 //   userName: "admin",
@@ -77,7 +355,8 @@ const back = () => {
   font-size: 38px;
   font-weight: bold;
   text-align: center;
-  line-height: 200px;
+  line-height: 100px;
+  margin-bottom: 60px;
 }
 .input-item {
   display: block;
@@ -104,6 +383,10 @@ const back = () => {
   text-align: center;
   line-height: 88px;
 }
+.msg2 {
+  line-height: 30px;
+
+}
 a {
   text-decoration-line: none;
   color: #abc1ee;
@@ -127,4 +410,55 @@ a {
   width: 80%;
   margin: 0 auto;
 }
+
+.form-wrapper2 {
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+
+/* 头像框 */
+
+
+
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 100%;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.avatar-uploader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  border: solid;
+  border-radius: 100%;
+
+
+}
+
+
+
+
 </style>
